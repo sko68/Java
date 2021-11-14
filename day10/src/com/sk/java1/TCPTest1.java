d2e6a5b5e8f919cce9146a57e499653caabb5df3
sko68 <sk16168@163.com>
2021-11-14 22:53:56 +0800

    更新成功

--- day10/src/com/sk/java1/TCPTest1.java
+++ day10/src/com/sk/java1/TCPTest1.java
@@ -1,5 +1,12 @@
 package com.sk.java1;
 
+import org.junit.Test;
+
+import java.io.*;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * @Description: TODO
  * @Author SK
@@ -8,4 +15,96 @@ package com.sk.java1;
  * @Version 1.0
  */
 public class TCPTest1 {
+    //客户端
+    @Test
+    public void client() {
+        Socket socket = null;
+        OutputStream os = null;
+        try {
+            //1.创建Socket对象。指明服务器端的ip和端口号
+            InetAddress inet = InetAddress.getByName("10.36.2.140");
+            socket = new Socket(inet, 16168);
+            //获取一个输出流，用于输出数据
+            os = socket.getOutputStream();
+            //3.写出数据的操作
+            os.write("您好，我是客户端MM".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (os != null) {
+
+                try {
+                    os.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null) {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    //服务端
+    @Test
+    public void server() {
+        ServerSocket ss = null;
+        Socket socket = null;
+        InputStream is = null;
+        try {
+            //1.创建服务端的ServerSocket，指明自己的端口号
+            ss = new ServerSocket(16168);
+            //2.调用accept()表示接收来自于客户端的socket
+            socket = ss.accept();
+            //3.获取输入流
+            is = socket.getInputStream();
+
+            //不建议这样写
+//            byte[] buffer = new byte[1024];
+//            int len;
+//            while ((len = is.read(buffer)) != -1) {
+//                String str = new String(buffer, 0, len);
+//                System.out.print(str);
+//            }
+            //4.读取输入流中的数据
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            byte[] buffer = new byte[5];
+            int len;
+            while ((len = is.read(buffer)) != -1) {
+                baos.write(buffer, 0, len);
+            }
+            System.out.println(baos.toString());
+            System.out.println("收到了来自：" + socket.getInetAddress().getHostAddress() + "的数据");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null) {
+
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (ss != null) {
+
+                try {
+                    ss.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
